f3b8ce837ef79fb9d3f798d01e3d529dab0f9468
RazorReddington <128988103+RazorReddington@users.noreply.github.com>
2025-04-26 18:34:09 +0100

    v2

--- ERM_Projection.py
+++ ERM_Projection.py
@@ -91,40 +91,105 @@ olb_proj = pd.DataFrame(olb_proj)
 
 
 
+
+
+
 #################### Projection - Scenario Dependent ####################
 runlist = list(master_input['Name'])
 
 
-#Needs to be scenario dependent
-#for run in runlist:
-    #hpi_table= master_input['HPI']
+######### Model Point Agnostic #############
+
+#HPI projection
+hpi = (1 + np.repeat(hpi_tables['base_HPI']['HPI'],freq))**(1/freq)
+hpi = np.cumprod(hpi)
     
-    
-    
+#Master Mortality Table
+
+mortality = mortality_tables['base_mortality']
+n = len(mortality)
+mortality['M'][0] = 0
+mortality['F'][0] = 0
+
+#convert to survival rates
+survival_female = [(1 - mortality['F'][i-1]) * (1- mortality['F'][i]) if i> 0 else 1 - mortality['F'][i] for i in range(n)]
+survival_male = [(1 - mortality['M'][i-1]) * (1- mortality['M'][i]) if i> 0 else 1 - mortality['M'][i] for i in range(n)]
+
+
+#reconstruct mortality decrements table
+male_mortality = [survival_male[i-1] - survival_male[i] if i >0 else 0 for  i in range(n) ]
+female_mortality = [survival_female[i-1] - survival_female[i] if i >0 else 0 for  i in range(n) ]
+mortality = pd.DataFrame({'Age': mortality['Age'],'M': male_mortality,'F': female_mortality})
+
+#Check
+mortality['F'].sum() == 1
+mortality['M'].sum() == 1
+
+
+#need to allow for settlement delay
+
+#need to extend size of table 
+
+
+
+#VER Projection
+ver = ver_tables['base_VER']
+
+n = len(ver)
+#convert to ver survival rates
+ver_survival_female = [(1 - ver['F'][i-1]) * (1- ver['F'][i]) if i> 0 else 1 - ver['F'][i] for i in range(n)]
+ver_survival_male = [(1 - ver['M'][i-1]) * (1- ver['M'][i]) if i> 0 else 1 - ver['M'][i] for i in range(n)]
+
+
+#reconstruct VER decrements table
+
+
+
+
+
+
+
+#Extend for frequency
+ver = ver.loc[ver.index.repeat(freq)]
+
+
 
 base_property_values = list(mpf['Loan Amount']/mpf['LTV'])
+
+
+#Cylce Through model points
 
 prop_test = []
 for i in range(len(mpf)):
     i=1
     property_value = base_property_values[i]
-    hpi = np.cumprod(1 + hpi_tables['base_HPI']['HPI'])
+    
     property_projection = property_value*hpi
     prop_test.append(property_projection)
     
     #Mortality
-    
     gender1 = mpf['Gender 1'][i]
     age1 = mpf['Age 1'][i]
-    mort = mortality_tables['base_mortality']
-    mort_array = mort[mort[gender1]]
     
-    index = np.array(range(0,proj_term))
-    age1_projection = pd.DataFrame(np.floor(age1 + index/freq), columns = ['Age'])
+    #calculation
     
-    test = pd.merge(age1_projection,mort,on='Age', how='left')
-    f = mort[mort['Age']==77][gender1]
-    g = mort.loc['Age']
+    current_olb = olb_proj[i]
+    current_mort = mortality.loc[mortality['Age']>=age1][gender1] #Slice the mortality table based on modelpoint demographic
+    current_mort.index = range(1,1 + len(current_mort)) #reset the index to start at 1
+    #need current mort to have for 1, then trailing 0's
+    
+    mort_income = current_olb * current_mort
+    
+    
+    #VER
+    current_ver = ver.loc[ver['Age']>=age1][gender1] #Slice the VER table based on modelpoint demographic
+    current_ver.index = range(1,1 + len(current_ver)) #reset the index to start at 1
+    
+    
+    #index = np.array(range(0,proj_term))
+    #age1_projection = pd.DataFrame(np.floor(age1 + index/freq), columns = ['Age'])
+    
+ 
     test = []
     for i in age1_projection:
         i=77
